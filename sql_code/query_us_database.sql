b8b784faba2240e1a3d5c07f94f6b3a3d514a3ea
tamos <tyler.amos@alumni.carleton.ca>
2018-11-28 13:20:42 -0600

    Update query_us_database.sql

--- sql_code/query_us_database.sql
+++ sql_code/query_us_database.sql
@@ -1,7 +1,11 @@
 -- query us database for the full set of data, inner join 
 -- usage: sqlite3 mydb.db < query_us_database.sql > my_desired_data_file.csv
 
-
+-- select all columns from a table which is the result of a join between speeches, descriptions, and speakers
+-- constraints: 
+--	dateof column in descriptions is greater than 19931231, i.e., 1994 onward
+-- 	chamber column in speakers is "H" 
+-- 	sort the results by date 
 SELECT * FROM speeches, descriptions, speakers
 			WHERE speeches.speech_id=speakers.speech_id
 			AND speakers.speech_id=descriptions.speech_id 
